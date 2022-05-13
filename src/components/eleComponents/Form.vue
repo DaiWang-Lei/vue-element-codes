f094e00cd05148b28c92ecad69235e20261b9336
Lei-Xixia <1377158174@qq.com>
2022-05-13 17:42:59 +0800

    feat : 修改错别字

--- src/components/eleComponents/Form.vue
+++ src/components/eleComponents/Form.vue
@@ -58,7 +58,7 @@
     <el-form-item>
       <el-button type="primary" @click="onSubmit">立即创建</el-button>
       <el-button>取消</el-button>
-      <el-button @click="resetForm('form')">重制</el-button>
+      <el-button @click="resetForm('form')">重置</el-button>
     </el-form-item>
   </el-form>
 </template>
