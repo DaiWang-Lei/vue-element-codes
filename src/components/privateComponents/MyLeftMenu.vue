601cf00eda3c67038ec4b172b4066652559aab3e
DaiWang <1403693160@qq.com>
2022-05-14 14:16:35 +0800

    feat：修改排列顺序

--- src/components/privateComponents/MyLeftMenu.vue
+++ src/components/privateComponents/MyLeftMenu.vue
@@ -9,19 +9,24 @@
     >
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">Form</span>
+          <i class="el-icon-eleme"></i>
+          <span slot="title">Element</span>
         </template>
         <el-menu-item-group>
-          <span slot="title">Input</span>
-          <router-link to="/input">
-            <el-menu-item index="1-1"> Input输入框 </el-menu-item>
-          </router-link>
+          <el-submenu index="1-1">
+            <span slot="title">Input</span>
+            <router-link to="/input">
+              <el-menu-item index="1-1"> Input输入框 </el-menu-item>
+            </router-link>
+          </el-submenu>
         </el-menu-item-group>
-        <el-menu-item-group title="Form">
-          <router-link to="/form">
-            <el-menu-item index="1-3"> Form表单 </el-menu-item>
-          </router-link>
+        <el-menu-item-group >
+          <el-submenu index="1-2">
+            <span slot="title">Form</span>
+            <router-link to="/form">
+              <el-menu-item index="1-3"> Form表单 </el-menu-item>
+            </router-link>
+          </el-submenu>
         </el-menu-item-group>
         <el-submenu index="1-4">
           <span slot="title">选项4</span>
